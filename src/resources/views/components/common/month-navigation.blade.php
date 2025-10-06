3833454598a69ce4a66845266953e62c02e7a7f0
daichi-hasegawa-styleedge <d.hasegawa@styleedge.co.jp>
2025-10-06 21:02:18 +0900

    UIの修正

--- src/resources/views/components/common/month-navigation.blade.php
+++ src/resources/views/components/common/month-navigation.blade.php
@@ -8,9 +8,11 @@
 <div class="month-navigation">
   <a href="{{ route($routeName, array_merge($routeParams, ['month' => $prevMonth->format('Y-m')])) }}" class="nav-arrow">
     <img src="{{ asset('images/arrow-left.svg') }}" alt="前月" class="arrow-icon">
+    前月
   </a>
   <span class="current-month">{{ $currentMonth->format('Y年m月') }}</span>
   <a href="{{ route($routeName, array_merge($routeParams, ['month' => $nextMonth->format('Y-m')])) }}" class="nav-arrow">
+    翌月
     <img src="{{ asset('images/arrow-right.svg') }}" alt="翌月" class="arrow-icon">
   </a>
 </div>
